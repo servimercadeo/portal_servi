16853f10e9747dfa003d84f652991d7c396bf628
Santiago <santiagogil271@gmail.com>
2022-03-10 09:50:40 -0500

    cambiando icono de la app

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,7 +11,7 @@
         <link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700" rel="stylesheet">
 
         <!-- Styles -->
-        <link rel="icon" href="{{ asset('imgs/icon.ico') }}">
+        <link rel="icon" href="{{ url('imgs/icon.ico') }}">
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
         <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
 
