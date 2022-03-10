73a796d86713fd39dc05f635463718fb3b4b439a
Santiago <santiagogil271@gmail.com>
2022-03-10 09:42:26 -0500

    cambiando icono de la app

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,7 +9,7 @@
         <link rel="icon" href="{{ asset('imgs/icon.ico') }}">
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700" rel="stylesheet">
+        {{-- <link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700" rel="stylesheet"> --}}
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
